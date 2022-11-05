e6219f4f2e148a725b08e9774758009d3c666f49
Chidi Williams <williamschidi1@gmail.com>
2022-11-05 18:01:00 +0000

    Fix GGML inference

--- __test_whisper_cpp.py
+++ __test_whisper_cpp.py
@@ -12,12 +12,12 @@ from whisper_cpp import (String, whisper_free, whisper_full,
                          whisper_full_n_segments, whisper_init)
 from whispr import download_whisper_cpp_model
 
-faulthandler.enable()
+# faulthandler.enable()
 
 if __name__ == "__main__":
     # Fixes opening new window when app has been frozen on Windows:
     # https://stackoverflow.com/a/33979091
-    multiprocessing.freeze_support()
+    # multiprocessing.freeze_support()
 
     model_path = download_whisper_cpp_model("tiny")
     ctx = whisper_init(String(model_path.encode('utf-8')))
