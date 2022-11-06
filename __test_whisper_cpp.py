2481f24474efe782d437d4057a48cad985e3da41
Chidi Williams <williamschidi1@gmail.com>
2022-11-06 02:01:02 +0000

    Fix GGML inference

--- __test_whisper_cpp.py
+++ __test_whisper_cpp.py
@@ -1,11 +1,15 @@
 
 import ctypes
 import faulthandler
+import hashlib
+import logging
+import os
 import pathlib
 
+import requests
 import whisper
-
-from whispr import download_whisper_cpp_model
+from appdirs import user_cache_dir
+from tqdm import tqdm
 
 faulthandler.enable()
 
@@ -41,6 +45,54 @@ whisper_cpp = ctypes.CDLL(
 whisper_cpp.whisper_init.restype = ctypes.c_void_p
 whisper_cpp.whisper_full_default_params.restype = WhisperFullParams
 whisper_cpp.whisper_full_get_segment_text.restype = ctypes.c_char_p
+
+MODELS_SHA256 = {
+    'tiny': 'be07e048e1e599ad46341c8d2a135645097a538221678b7acdd1b1919c6e1b21',
+    'base': '60ed5bc3dd14eea856493d334349b405782ddcaf0028d4b5df4088345fba2efe',
+    'small': '1be3a9b2063867b937e64e2ec7483364a79917e157fa98c5d94b5c1fffea987b',
+}
+
+
+def download_whisper_cpp_model(name: str):
+    """Downloads a Whisper.cpp GGML model to the user cache directory."""
+
+    base_dir = user_cache_dir('Buzz')
+    os.makedirs(base_dir, exist_ok=True)
+
+    model_path = os.path.join(
+        base_dir, f'ggml-model-whisper-{name}.bin')
+
+    if os.path.exists(model_path) and not os.path.isfile(model_path):
+        raise RuntimeError(
+            f"{model_path} exists and is not a regular file")
+
+    expected_sha256 = MODELS_SHA256[name]
+
+    if os.path.isfile(model_path):
+        model_bytes = open(model_path, "rb").read()
+        if hashlib.sha256(model_bytes).hexdigest() == expected_sha256:
+            return model_path
+
+        logging.debug(
+            '%s exists, but the SHA256 checksum does not match; re-downloading the file', model_path)
+
+    url = f'https://ggml.buzz.chidiwilliams.com/ggml-model-whisper-{name}.bin'
+    with requests.get(url, stream=True, timeout=15) as source, open(model_path, 'wb') as output:
+        source.raise_for_status()
+
+        total_size = int(source.headers.get('Content-Length', 0))
+        with tqdm(total=total_size, ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop:
+            for chunk in source.iter_content(chunk_size=8192):
+                output.write(chunk)
+                loop.update(len(chunk))
+
+    model_bytes = open(model_path, "rb").read()
+    if hashlib.sha256(model_bytes).hexdigest() != expected_sha256:
+        raise RuntimeError(
+            "Model has been downloaded but the SHA256 checksum does not match. Please retry loading the model.")
+
+    return model_path
+
 
 if __name__ == "__main__":
     # Fixes opening new window when app has been frozen on Windows:
