6785586c4a975e362063776ed8634c065fb8bdf6
Chidi Williams <williamschidi1@gmail.com>
2022-11-12 15:40:48 +0000

    Add stable timings

--- whispr.py
+++ whispr.py
@@ -38,12 +38,17 @@ class Task(enum.Enum):
     TRANSCRIBE = "transcribe"
 
 
-def whisper_cpp_params(language: str, task: Task, print_realtime=False, print_progress=False):
-    params = whisper_cpp.whisper_full_default_params(0)
+def whisper_cpp_params(
+        language: str, task: Task, word_level_timings: bool,
+        print_realtime=False, print_progress=False,):
+    params = whisper_cpp.whisper_full_default_params(whisper_cpp.WHISPER_SAMPLING_GREEDY)
     params.print_realtime = print_realtime
     params.print_progress = print_progress
     params.language = whisper_cpp.String(language.encode('utf-8'))
     params.translate = task == Task.TRANSLATE
+    params.max_len = ctypes.c_int(1)
+    params.max_len = 1 if word_level_timings else 0
+    params.token_timestamps = word_level_timings
     return params
 
 
@@ -85,7 +90,6 @@ class WhisperCpp:
         whisper_cpp.whisper_free((self.ctx))
 
 
-# TODO: should this instead subclass Process?
 class ModelLoader:
     process: multiprocessing.Process
     model_path_queue: multiprocessing.Queue
@@ -133,16 +137,12 @@ class ModelLoader:
         return WhisperCpp(model_path) if self.use_whisper_cpp else whisper.load_model(model_path)
 
     def load_whisper_cpp_model(self, stderr_conn: Connection, queue: multiprocessing.Queue, name: str):
-        path = download_whisper_cpp_model(name)
+        path = download_model(name, use_whisper_cpp=True)
         queue.put(path)
 
     def load_whisper_model(self, stderr_conn: Connection, queue: multiprocessing.Queue, name: str):
         with pipe_stderr(stderr_conn):
-            download_root = os.getenv(
-                "XDG_CACHE_HOME",
-                os.path.join(os.path.expanduser("~"), ".cache", "whisper")
-            )
-            path = download_whisper_model(whisper._MODELS[name], download_root)
+            path = download_model(name, use_whisper_cpp=False)
             queue.put(path)
 
     def stop(self):
@@ -153,43 +153,6 @@ class ModelLoader:
         return self.process.is_alive()
 
 
-def download_whisper_model(url: str, root: str):
-    """See whisper._download"""
-    os.makedirs(root, exist_ok=True)
-
-    expected_sha256 = url.split("/")[-2]
-    download_target = os.path.join(root, os.path.basename(url))
-
-    if os.path.exists(download_target) and not os.path.isfile(download_target):
-        raise RuntimeError(
-            f"{download_target} exists and is not a regular file")
-
-    if os.path.isfile(download_target):
-        model_bytes = open(download_target, "rb").read()
-        if hashlib.sha256(model_bytes).hexdigest() == expected_sha256:
-            return download_target
-        else:
-            warnings.warn(
-                f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file")
-
-    # Downloads the model using the requests module instead of urllib to
-    # use the certs from certifi when the app is running in frozen mode
-    with requests.get(url, stream=True, timeout=15) as source, open(download_target, 'wb') as output:
-        source.raise_for_status()
-        total_size = int(source.headers.get('Content-Length', 0))
-        with tqdm(total=total_size, ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop:
-            for chunk in source.iter_content(chunk_size=8192):
-                output.write(chunk)
-                loop.update(len(chunk))
-
-    model_bytes = open(download_target, "rb").read()
-    if hashlib.sha256(model_bytes).hexdigest() != expected_sha256:
-        raise RuntimeError(
-            "Model has been downloaded but the SHA256 checksum does not not match. Please retry loading the model.")
-
-    return download_target
-
-
 MODELS_SHA256 = {
     'tiny': 'be07e048e1e599ad46341c8d2a135645097a538221678b7acdd1b1919c6e1b21',
     'base': '60ed5bc3dd14eea856493d334349b405782ddcaf0028d4b5df4088345fba2efe',
@@ -197,33 +160,39 @@ MODELS_SHA256 = {
 }
 
 
-def download_whisper_cpp_model(name: str):
-    """Downloads a Whisper.cpp GGML model to the user cache directory."""
+def download_model(name: str, use_whisper_cpp=False):
+    if use_whisper_cpp:
+        root = user_cache_dir('Buzz')
+        url = f'https://ggml.buzz.chidiwilliams.com/ggml-model-whisper-{name}.bin'
+    else:
+        root = os.getenv(
+            "XDG_CACHE_HOME",
+            os.path.join(os.path.expanduser("~"), ".cache", "whisper")
+        )
+        url = whisper._MODELS[name]
 
-    base_dir = user_cache_dir('Buzz')
-    os.makedirs(base_dir, exist_ok=True)
+    os.makedirs(root, exist_ok=True)
 
-    model_path = os.path.join(
-        base_dir, f'ggml-model-whisper-{name}.bin')
+    model_path = os.path.join(root, os.path.basename(url))
 
     if os.path.exists(model_path) and not os.path.isfile(model_path):
         raise RuntimeError(
             f"{model_path} exists and is not a regular file")
 
-    expected_sha256 = MODELS_SHA256[name]
-
+    expected_sha256 = MODELS_SHA256[name] if use_whisper_cpp else url.split(
+        "/")[-2]
     if os.path.isfile(model_path):
         model_bytes = open(model_path, "rb").read()
         if hashlib.sha256(model_bytes).hexdigest() == expected_sha256:
             return model_path
+        else:
+            warnings.warn(
+                f"{model_path} exists, but the SHA256 checksum does not match; re-downloading the file")
 
-        logging.debug(
-            '%s exists, but the SHA256 checksum does not match; re-downloading the file', model_path)
-
-    url = f'https://ggml.buzz.chidiwilliams.com/ggml-model-whisper-{name}.bin'
+    # Downloads the model using the requests module instead of urllib to
+    # use the certs from certifi when the app is running in frozen mode
     with requests.get(url, stream=True, timeout=15) as source, open(model_path, 'wb') as output:
         source.raise_for_status()
-
         total_size = int(source.headers.get('Content-Length', 0))
         with tqdm(total=total_size, ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop:
             for chunk in source.iter_content(chunk_size=8192):
