fd752ed1ad9f9349f0b945d662cf7dd3451acde1
Chidi Williams <williamschidi1@gmail.com>
2022-12-04 19:58:43 +0000

    Fix Windows build

--- buzz/transcriber.py
+++ buzz/transcriber.py
@@ -263,7 +263,7 @@ class WhisperCppFileTranscriber(QRunnable):
 
         logging.debug('Running whisper_cpp process, args = %s', args)
 
-        self.process.start('whisper_cpp', args)
+        self.process.start('./whisper_cpp', args)
 
     def on_process_finished(self):
         status = self.process.exitStatus()
