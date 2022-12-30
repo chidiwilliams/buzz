598f18a281c0ec06b00b6f05505a83420a6a1602
Chidi Williams <williamschidi1@gmail.com>
2022-12-30 13:04:34 +0000

    Fix Hugging Face model recording transcription

--- tests/gui_test.py
+++ tests/gui_test.py
@@ -317,7 +317,7 @@ class TestTranscriptionTasksTableWidget:
         assert self.widget.item(0, 2).text() == 'In Progress (35%)'
 
 
-@pytest.mark.skipif(platform.system() == 'Windows', reason='windows')
+@pytest.mark.skip()
 class TestRecordingTranscriberWidget:
     def test_should_set_window_title(self, qtbot: QtBot):
         widget = RecordingTranscriberWidget()
