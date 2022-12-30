a842ad5aee672d3b52440cd87af8f9d4e6123191
Chidi Williams <williamschidi1@gmail.com>
2022-12-30 16:37:39 +0000

    Fix transformers package loading __init__.py dynamically

--- buzz/gui.py
+++ buzz/gui.py
@@ -625,7 +625,7 @@ class RecordingTranscriberWidget(QWidget):
 def get_asset_path(path: str):
     if getattr(sys, 'frozen', False):
         return os.path.join(os.path.dirname(sys.executable), path)
-    return os.path.join(__file__, '..', path)
+    return os.path.join(os.path.dirname(__file__), '..', path)
 
 
 BUZZ_ICON_PATH = get_asset_path('assets/buzz.ico')
