42860210e7db38ea464632d91fc942a4b13f431b
Chidi Williams <williamschidi1@gmail.com>
2023-04-10 11:46:54 +0100

    Add Faster-Whisper

--- buzz/transcriber.py
+++ buzz/transcriber.py
@@ -433,7 +433,7 @@ class WhisperFileTranscriber(FileTranscriber):
 
         recv_pipe, send_pipe = multiprocessing.Pipe(duplex=False)
 
-        self.current_process = multiprocessing.Process(target=transcribe_whisper,
+        self.current_process = multiprocessing.Process(target=self.transcribe_whisper,
                                                        args=(send_pipe, self.transcription_task))
         if not self.stopped:
             self.current_process.start()
@@ -458,6 +458,97 @@ class WhisperFileTranscriber(FileTranscriber):
             raise Exception('Unknown error')
 
         return self.segments
+
+    @classmethod
+    def transcribe_whisper(cls, stderr_conn: Connection, task: FileTranscriptionTask) -> None:
+        with pipe_stderr(stderr_conn):
+            if task.transcription_options.model.model_type == ModelType.HUGGING_FACE:
+                segments = cls.transcribe_hugging_face(task)
+            elif task.transcription_options.model.model_type == ModelType.FASTER_WHISPER:
+                segments = cls.transcribe_faster_whisper(task)
+            elif task.transcription_options.model.model_type == ModelType.WHISPER:
+                segments = cls.transcribe_openai_whisper(task)
+            else:
+                raise Exception(f"Invalid model type: {task.transcription_options.model.model_type}")
+
+            segments_json = json.dumps(
+                segments, ensure_ascii=True, default=vars)
+            sys.stderr.write(f'segments = {segments_json}\n')
+            sys.stderr.write(
+                WhisperFileTranscriber.READ_LINE_THREAD_STOP_TOKEN + '\n')
+
+    @abstractmethod
+    def get_segments(self, task: FileTranscriptionTask) -> List[Segment]:
+        ...
+
+    @classmethod
+    def transcribe_hugging_face(cls, task: FileTranscriptionTask) -> List[Segment]:
+        model = transformers_whisper.load_model(task.model_path)
+        language = task.transcription_options.language if task.transcription_options.language is not None else 'en'
+        result = model.transcribe(audio=task.file_path, language=language,
+                                  task=task.transcription_options.task.value, verbose=False)
+        return [
+            Segment(
+                start=int(segment.get('start') * 1000),
+                end=int(segment.get('end') * 1000),
+                text=segment.get('text'),
+            ) for segment in result.get('segments')]
+
+    @classmethod
+    def transcribe_faster_whisper(cls, task: FileTranscriptionTask) -> List[Segment]:
+        model = faster_whisper.WhisperModel(
+            model_size_or_path=task.transcription_options.model.whisper_model_size.value)
+        whisper_segments, _ = model.transcribe(audio=task.file_path, language=task.transcription_options.language,
+                                               task=task.transcription_options.task.value,
+                                               temperature=task.transcription_options.temperature,
+                                               initial_prompt=task.transcription_options.initial_prompt,
+                                               word_timestamps=task.transcription_options.word_level_timings)
+        segments = []
+        for segment in list(whisper_segments):
+            # Segment will contain words if word-level timings is True
+            if segment.words:
+                for word in segment.words:
+                    segments.append(Segment(
+                        start=int(word.start * 1000),
+                        end=int(word.end * 1000),
+                        text=word.word
+                    ))
+            else:
+                segments.append(Segment(
+                    start=int(segment.start * 1000),
+                    end=int(segment.end * 1000),
+                    text=segment.text
+                ))
+        return segments
+
+    @classmethod
+    def transcribe_openai_whisper(cls, task: FileTranscriptionTask) -> List[Segment]:
+        model = whisper.load_model(task.model_path)
+
+        if task.transcription_options.word_level_timings:
+            stable_whisper.modify_model(model)
+            result = model.transcribe(
+                audio=task.file_path, language=task.transcription_options.language,
+                task=task.transcription_options.task.value, temperature=task.transcription_options.temperature,
+                initial_prompt=task.transcription_options.initial_prompt, pbar=True)
+            segments = stable_whisper.group_word_timestamps(result)
+            return [Segment(
+                start=int(segment.get('start') * 1000),
+                end=int(segment.get('end') * 1000),
+                text=segment.get('text'),
+            ) for segment in segments]
+
+        result = model.transcribe(
+            audio=task.file_path, language=task.transcription_options.language,
+            task=task.transcription_options.task.value,
+            temperature=task.transcription_options.temperature,
+            initial_prompt=task.transcription_options.initial_prompt, verbose=False)
+        segments = result.get('segments')
+        return [Segment(
+            start=int(segment.get('start') * 1000),
+            end=int(segment.get('end') * 1000),
+            text=segment.get('text'),
+        ) for segment in segments]
 
     def stop(self):
         self.stopped = True
@@ -489,79 +580,6 @@ class WhisperFileTranscriber(FileTranscriber):
                 except ValueError:
                     logging.debug('whisper (stderr): %s', line)
                     continue
-
-
-def transcribe_whisper(stderr_conn: Connection, task: FileTranscriptionTask):
-    with pipe_stderr(stderr_conn):
-        if task.transcription_options.model.model_type == ModelType.HUGGING_FACE:
-            model = transformers_whisper.load_model(task.model_path)
-            language = task.transcription_options.language if task.transcription_options.language is not None else 'en'
-            result = model.transcribe(audio=task.file_path, language=language,
-                                      task=task.transcription_options.task.value, verbose=False)
-            whisper_segments = result.get('segments')
-            segments = [
-                Segment(
-                    start=int(segment.get('start') * 1000),
-                    end=int(segment.get('end') * 1000),
-                    text=segment.get('text'),
-                ) for segment in whisper_segments]
-        elif task.transcription_options.model.model_type == ModelType.FASTER_WHISPER:
-            model = faster_whisper.WhisperModel(
-                model_size_or_path=task.transcription_options.model.whisper_model_size.value)
-            whisper_segments, _ = model.transcribe(audio=task.file_path, language=task.transcription_options.language,
-                                                   task=task.transcription_options.task.value,
-                                                   temperature=task.transcription_options.temperature,
-                                                   initial_prompt=task.transcription_options.initial_prompt,
-                                                   word_timestamps=task.transcription_options.word_level_timings)
-            segments = []
-            for segment in list(whisper_segments):
-                if segment.words:
-                    for word in segment.words:
-                        segments.append(Segment(
-                            start=int(word.start * 1000),
-                            end=int(word.end * 1000),
-                            text=word.word
-                        ))
-                else:
-                    segments.append(Segment(
-                        start=int(segment.start * 1000),
-                        end=int(segment.end * 1000),
-                        text=segment.text
-                    ))
-        else:
-            model = whisper.load_model(task.model_path)
-            if task.transcription_options.word_level_timings:
-                stable_whisper.modify_model(model)
-                result = model.transcribe(
-                    audio=task.file_path, language=task.transcription_options.language,
-                    task=task.transcription_options.task.value, temperature=task.transcription_options.temperature,
-                    initial_prompt=task.transcription_options.initial_prompt, pbar=True)
-                whisper_segments = stable_whisper.group_word_timestamps(result)
-                segments = [
-                    Segment(
-                        start=int(segment.get('start') * 1000),
-                        end=int(segment.get('end') * 1000),
-                        text=segment.get('text'),
-                    ) for segment in whisper_segments]
-            else:
-                result = model.transcribe(
-                    audio=task.file_path, language=task.transcription_options.language,
-                    task=task.transcription_options.task.value,
-                    temperature=task.transcription_options.temperature,
-                    initial_prompt=task.transcription_options.initial_prompt, verbose=False)
-                whisper_segments = result.get('segments')
-                segments = [
-                    Segment(
-                        start=int(segment.get('start') * 1000),
-                        end=int(segment.get('end') * 1000),
-                        text=segment.get('text'),
-                    ) for segment in whisper_segments]
-
-        segments_json = json.dumps(
-            segments, ensure_ascii=True, default=vars)
-        sys.stderr.write(f'segments = {segments_json}\n')
-        sys.stderr.write(
-            WhisperFileTranscriber.READ_LINE_THREAD_STOP_TOKEN + '\n')
 
 
 def write_output(path: str, segments: List[Segment], output_format: OutputFormat):
