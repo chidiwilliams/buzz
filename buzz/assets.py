dfa59896c13a9f4675dc736666d4c80452658c99
Chidi Williams <williamschidi1@gmail.com>
2023-04-22 23:07:26 +0100

    Add keyboard shortcuts

---
+++ buzz/assets.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+
+def get_asset_path(path: str):
+    if getattr(sys, 'frozen', False):
+        return os.path.join(os.path.dirname(sys.executable), path)
+    return os.path.join(os.path.dirname(__file__), '..', path)
