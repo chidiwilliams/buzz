39a26af7dc1220ffe5ec3033fb1da427c3409e88
Chidi Williams <williamschidi1@gmail.com>
2023-04-28 19:25:17 +0100

    Add models preferences

--- tests/widgets/models_preferences_widget_test.py
+++ tests/widgets/models_preferences_widget_test.py
@@ -1,3 +1,5 @@
+from PyQt6.QtWidgets import QComboBox
+
 from buzz.widgets.models_preferences_widget import ModelsPreferencesWidget
 
 
@@ -5,3 +7,23 @@ class TestModelsPreferencesWidget:
     def test_should_show_model_list(self, qtbot):
         widget = ModelsPreferencesWidget()
         qtbot.add_widget(widget)
+
+        first_item = widget.model_list_widget.topLevelItem(0)
+        assert first_item.text(0) == 'Downloaded'
+
+        second_item = widget.model_list_widget.topLevelItem(1)
+        assert second_item.text(0) == 'Available for Download'
+
+    def test_should_change_model_type(self, qtbot):
+        widget = ModelsPreferencesWidget()
+        qtbot.add_widget(widget)
+
+        combo_box = widget.findChild(QComboBox)
+        assert isinstance(combo_box, QComboBox)
+        combo_box.setCurrentText('Faster Whisper')
+
+        first_item = widget.model_list_widget.topLevelItem(0)
+        assert first_item.text(0) == 'Downloaded'
+
+        second_item = widget.model_list_widget.topLevelItem(1)
+        assert second_item.text(0) == 'Available for Download'
