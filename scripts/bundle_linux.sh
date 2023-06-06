35c64432e243a3f728632da4bcb9695a48cb4c45
Chidi Williams <williamschidi1@gmail.com>
2023-06-06 08:37:30 +0100

    Add FPM

--- scripts/bundle_linux.sh
+++ scripts/bundle_linux.sh
@@ -1,12 +1,19 @@
+# https://www.pythonguis.com/tutorials/packaging-pyqt5-applications-linux-pyinstaller/
+
 PYINSTALLER_BUILD_PATH="dist/Buzz"
 PACKAGE_PATH="dist/package"
 
 mkdir -p $PACKAGE_PATH/opt
 mkdir -p $PACKAGE_PATH/usr/share/applications
 mkdir -p $PACKAGE_PATH/usr/share/icons/hicolor/scalable/apps
+
 cp -r $PYINSTALLER_BUILD_PATH $PACKAGE_PATH/opt/buzz
 cp buzz.desktop $PACKAGE_PATH/usr/share/applications
 
+# Copy icons
+cp assets/buzz.svg $PACKAGE_PATH/usr/share/icons/hicolor/scalable/apps/buzz.svg
+
+# Set permissions
 find $PACKAGE_PATH/opt/buzz -type f -exec chmod 644 -- {} +
 find $PACKAGE_PATH/opt/buzz -type d -exec chmod 755 -- {} +
 find $PACKAGE_PATH/usr/share -type f -exec chmod 644 -- {} +
