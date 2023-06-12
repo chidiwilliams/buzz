0441e10872d0ed531d73bc88cbb084eb347eb36b
Chidi Williams <williamschidi1@gmail.com>
2023-06-12 10:11:49 +0100

    Fix app build

--- build.py
+++ build.py
@@ -1,8 +1,19 @@
+import platform
 import subprocess
+
+if platform.system() == 'Darwin':
+    libwhisper = 'libwhisper.dylib'
+elif platform.system() == 'Windows':
+    libwhisper = 'whisper.dll'
+elif platform.system() == 'Linux':
+    libwhisper = 'libwhisper.so'
 
 
 def build(setup_kwargs):
-    subprocess.call(['make', 'buzz/whisper_cpp.py'])
+    subprocess.call(['make', 'whisper_cpp'])
+    subprocess.call(['ctypesgen', './whisper.cpp/whisper.h', f'-l{libwhisper}', '-o', 'buzz/whisper_cpp.py'])
+    raise Exception('Failed')
+    print('Written buzz/whisper_cpp.py')
 
 
 if __name__ == "__main__":
