f950069c441083c3c7e0a5622a70189b95e36326
chidiwilliams <chidiwilliams@users.noreply.github.com>
2023-06-20 13:56:10 +0000

    deploy: 758e3ece6783fb6043421b81617ca2b16c266c4b

--- buzz/assets.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-
-def get_asset_path(path: str):
-    if getattr(sys, 'frozen', False):
-        return os.path.join(os.path.dirname(sys.executable), path)
-    return os.path.join(os.path.dirname(__file__), '..', path)
