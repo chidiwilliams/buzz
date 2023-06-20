4e63e6c89d7630d8895b6d85d93de498f9174467
github-action-benchmark <github@users.noreply.github.com>
2023-06-20 14:21:03 +0000

    add macOS (pytest) benchmark result for b68663f324db16628533e3e74ed976c02946c97c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687270723322,
+  "lastUpdate": 1687270863654,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "Linux": [
@@ -90,6 +90,46 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0976441748162625",
             "extra": "mean: 13.269080507199988 sec\nrounds: 5"
+          }
+        ]
+      }
+    ],
+    "macOS": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b68663f324db16628533e3e74ed976c02946c97c",
+          "message": "Add docs (#500)",
+          "timestamp": "2023-06-20T14:12:18Z",
+          "tree_id": "52bf1699ed89cda87bdd4c1be3e87eab892b62c7",
+          "url": "https://github.com/chidiwilliams/buzz/commit/b68663f324db16628533e3e74ed976c02946c97c"
+        },
+        "date": 1687270858905,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.06227221478003078,
+            "unit": "iter/sec",
+            "range": "stddev: 6.590470067733014",
+            "extra": "mean: 16.058526319200006 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.03295215064586843,
+            "unit": "iter/sec",
+            "range": "stddev: 0.6669151205332262",
+            "extra": "mean: 30.347032906799996 sec\nrounds: 5"
           }
         ]
       }
