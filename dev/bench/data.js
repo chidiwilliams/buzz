2f3f39b704ad3d494f9682bf2874d036793ed814
github-action-benchmark <github@users.noreply.github.com>
2023-06-23 09:52:01 +0000

    add macOS (pytest) benchmark result for fa08e5344e7b400b4afd10c692dbaedea1aba2c8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687513918845,
+  "lastUpdate": 1687513921708,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "Linux": [
@@ -95,6 +95,44 @@ window.BENCHMARK_DATA = {
       }
     ],
     "macOS": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fa08e5344e7b400b4afd10c692dbaedea1aba2c8",
+          "message": "Add installation docs (#506)",
+          "timestamp": "2023-06-23T10:43:29+01:00",
+          "tree_id": "e365c9157f4a2faf7a94e03b81771991fd3a873a",
+          "url": "https://github.com/chidiwilliams/buzz/commit/fa08e5344e7b400b4afd10c692dbaedea1aba2c8"
+        },
+        "date": 1687513912208,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.10435626159015278,
+            "unit": "iter/sec",
+            "range": "stddev: 0.04890679815868742",
+            "extra": "mean: 9.582558676999998 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.04428217083212285,
+            "unit": "iter/sec",
+            "range": "stddev: 0.058766831056869896",
+            "extra": "mean: 22.582452061600087 sec\nrounds: 5"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
