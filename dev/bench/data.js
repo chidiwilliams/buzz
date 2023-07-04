3837418d519f44b0aeb39b7f661079c8afc6e0a6
github-action-benchmark <github@users.noreply.github.com>
2023-07-04 22:20:08 +0000

    add Windows (pytest) benchmark result for 59d9dc224318e0ea6f336ddf1a6afcda1dd59de7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688509204684,
+  "lastUpdate": 1688509208293,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "Linux": [
@@ -173,6 +173,51 @@ window.BENCHMARK_DATA = {
       }
     ],
     "Windows": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "59d9dc224318e0ea6f336ddf1a6afcda1dd59de7",
+          "message": "Fix Faster Whisper large model selection (#523)",
+          "timestamp": "2023-07-04T21:52:45Z",
+          "tree_id": "76600f4e77363d262ac1d18a7d34c1f6aff8f6be",
+          "url": "https://github.com/chidiwilliams/buzz/commit/59d9dc224318e0ea6f336ddf1a6afcda1dd59de7"
+        },
+        "date": 1688509192125,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.006898403348844147,
+            "unit": "iter/sec",
+            "range": "stddev: 0.23961586724333964",
+            "extra": "mean: 144.96108003999993 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.04766124706876892,
+            "unit": "iter/sec",
+            "range": "stddev: 0.11217622479228419",
+            "extra": "mean: 20.981406520000018 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Faster Whisper - Tiny]",
+            "value": 0.055508345276166465,
+            "unit": "iter/sec",
+            "range": "stddev: 0.1657682131349226",
+            "extra": "mean: 18.015309140000046 sec\nrounds: 5"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
