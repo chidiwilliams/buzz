5b1d3442ea11c57d3b0bbef582a4ace0b72ecfd9
github-action-benchmark <github@users.noreply.github.com>
2023-07-04 21:59:38 +0000

    add macOS (pytest) benchmark result for 59d9dc224318e0ea6f336ddf1a6afcda1dd59de7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688507929062,
+  "lastUpdate": 1688507978428,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "Linux": [
@@ -90,6 +90,46 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.09732181650581044",
             "extra": "mean: 13.4615994108 sec\nrounds: 5"
+          }
+        ]
+      }
+    ],
+    "macOS": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "59d9dc224318e0ea6f336ddf1a6afcda1dd59de7",
+          "message": "Fix Faster Whisper large model selection (#523)",
+          "timestamp": "2023-07-04T21:52:45Z",
+          "tree_id": "76600f4e77363d262ac1d18a7d34c1f6aff8f6be",
+          "url": "https://github.com/chidiwilliams/buzz/commit/59d9dc224318e0ea6f336ddf1a6afcda1dd59de7"
+        },
+        "date": 1688507973948,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.11192269388291283,
+            "unit": "iter/sec",
+            "range": "stddev: 0.04384517902261076",
+            "extra": "mean: 8.934738481600016 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.046474037231515906,
+            "unit": "iter/sec",
+            "range": "stddev: 0.3502882125582603",
+            "extra": "mean: 21.51739034459997 sec\nrounds: 5"
           }
         ]
       }
