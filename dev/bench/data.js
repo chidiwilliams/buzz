e7f28a60b1a49a060c600f951360267e2cce0cbd
github-action-benchmark <github@users.noreply.github.com>
2023-08-02 22:25:07 +0000

    add macOS (pytest) benchmark result for f1550f80adcc5fa741dca7199e9bae69a2d37187

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691015101131,
+  "lastUpdate": 1691015107405,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "Linux": [
@@ -95,6 +95,44 @@ window.BENCHMARK_DATA = {
       }
     ],
     "macOS": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f1550f80adcc5fa741dca7199e9bae69a2d37187",
+          "message": "Update translations (#556)",
+          "timestamp": "2023-08-02T22:15:59Z",
+          "tree_id": "3fad0ad83857424ea31d61befd48dc94fccd9a13",
+          "url": "https://github.com/chidiwilliams/buzz/commit/f1550f80adcc5fa741dca7199e9bae69a2d37187"
+        },
+        "date": 1691015093779,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.044345755196943656,
+            "unit": "iter/sec",
+            "range": "stddev: 3.727354377006096",
+            "extra": "mean: 22.550072618200012 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.04298650892686658,
+            "unit": "iter/sec",
+            "range": "stddev: 0.9598295156815584",
+            "extra": "mean: 23.263112659399972 sec\nrounds: 5"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
