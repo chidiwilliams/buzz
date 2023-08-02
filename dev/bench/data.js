899f172d7bebdaf7f949ebb2f2e9e178c8146ee7
github-action-benchmark <github@users.noreply.github.com>
2023-08-02 22:23:25 +0000

    add Linux (pytest) benchmark result for f1550f80adcc5fa741dca7199e9bae69a2d37187

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,8 +1,53 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691015004441,
+  "lastUpdate": 1691015005550,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "Linux": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f1550f80adcc5fa741dca7199e9bae69a2d37187",
+          "message": "Update translations (#556)",
+          "timestamp": "2023-08-02T22:15:59Z",
+          "tree_id": "3fad0ad83857424ea31d61befd48dc94fccd9a13",
+          "url": "https://github.com/chidiwilliams/buzz/commit/f1550f80adcc5fa741dca7199e9bae69a2d37187"
+        },
+        "date": 1691015001442,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.0647561902009507,
+            "unit": "iter/sec",
+            "range": "stddev: 0.31012694368520805",
+            "extra": "mean: 15.442539113200002 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.0931942210528717,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0693337718669276",
+            "extra": "mean: 10.730279074200018 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Faster Whisper - Tiny]",
+            "value": 0.06744246650951953,
+            "unit": "iter/sec",
+            "range": "stddev: 0.2792091074603467",
+            "extra": "mean: 14.827452964799999 sec\nrounds: 5"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
