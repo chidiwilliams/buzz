658674b5476c02379c278c03893a2ebd526d802b
github-action-benchmark <github@users.noreply.github.com>
2023-08-03 04:49:29 +0000

    add Linux (pytest) benchmark result for af4d57b8814ae0654cb9244f6c24425979a18c3a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,8 +1,53 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691038167852,
+  "lastUpdate": 1691038169764,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "Linux": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "af4d57b8814ae0654cb9244f6c24425979a18c3a",
+          "message": "Add delete model and show model location options (#557)",
+          "timestamp": "2023-08-03T04:41:59Z",
+          "tree_id": "539e55dfba88cd75cc1e82875e81cc332d34edf1",
+          "url": "https://github.com/chidiwilliams/buzz/commit/af4d57b8814ae0654cb9244f6c24425979a18c3a"
+        },
+        "date": 1691038162955,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.061539176632782897,
+            "unit": "iter/sec",
+            "range": "stddev: 0.10900204125434998",
+            "extra": "mean: 16.249811172599994 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.10032540927735613,
+            "unit": "iter/sec",
+            "range": "stddev: 0.13701472652586344",
+            "extra": "mean: 9.967564620000052 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Faster Whisper - Tiny]",
+            "value": 0.06574246681526988,
+            "unit": "iter/sec",
+            "range": "stddev: 0.1538324496553993",
+            "extra": "mean: 15.210868232400003 sec\nrounds: 5"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
