bdbcc43b1207efaf11f14cae01cf99c4e8490816
github-action-benchmark <github@users.noreply.github.com>
2023-08-19 01:58:26 +0000

    add Windows (pytest) benchmark result for f29b3aa521c1a38e24a7a292d359ce59758fd811

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692410301347,
+  "lastUpdate": 1692410306132,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "macOS": [
@@ -81,6 +81,51 @@ window.BENCHMARK_DATA = {
       }
     ],
     "Windows": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f29b3aa521c1a38e24a7a292d359ce59758fd811",
+          "message": "Add captions to audio player (#573)",
+          "timestamp": "2023-08-19T01:29:13Z",
+          "tree_id": "45071cf011d3404734b2c2c757588d53f9656ef0",
+          "url": "https://github.com/chidiwilliams/buzz/commit/f29b3aa521c1a38e24a7a292d359ce59758fd811"
+        },
+        "date": 1692410286384,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.0066327171499216304,
+            "unit": "iter/sec",
+            "range": "stddev: 0.4640179813400409",
+            "extra": "mean: 150.76777396000003 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.046031602350309855,
+            "unit": "iter/sec",
+            "range": "stddev: 1.0519295196549092",
+            "extra": "mean: 21.724205740000023 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Faster Whisper - Tiny]",
+            "value": 0.055454114145552846,
+            "unit": "iter/sec",
+            "range": "stddev: 0.10751288754686195",
+            "extra": "mean: 18.032927140000037 sec\nrounds: 5"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
