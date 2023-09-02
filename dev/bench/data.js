e64575af15a3c9020c21343c8813d92b79809d06
github-action-benchmark <github@users.noreply.github.com>
2023-09-02 13:38:43 +0000

    add macOS (pytest) benchmark result for 4587cd55f0d2a264a316ee753e87c99a9b3215b5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,8 +1,46 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1693661922030,
+  "lastUpdate": 1693661923822,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "macOS": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4587cd55f0d2a264a316ee753e87c99a9b3215b5",
+          "message": "Fix translator (#591)",
+          "timestamp": "2023-09-02T13:30:23Z",
+          "tree_id": "a29f8d28fc4eca404776d5bcde7024a58cfa41ad",
+          "url": "https://github.com/chidiwilliams/buzz/commit/4587cd55f0d2a264a316ee753e87c99a9b3215b5"
+        },
+        "date": 1693661917291,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.1291248952367305,
+            "unit": "iter/sec",
+            "range": "stddev: 0.06435446237023491",
+            "extra": "mean: 7.744439971600014 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.0450250183979397,
+            "unit": "iter/sec",
+            "range": "stddev: 0.23388686097791572",
+            "extra": "mean: 22.209874322800033 sec\nrounds: 5"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
