ac203394fd5a49edaaa6ce1d738b5e0574f880e5
github-action-benchmark <github@users.noreply.github.com>
2023-09-19 20:16:38 +0000

    add Windows (pytest) benchmark result for a3b54891e8c403d28b7bf2d86443ad310d2d1b51

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695153945146,
+  "lastUpdate": 1695154598168,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "macOS": [
@@ -76,6 +76,53 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.6175114828270604",
             "extra": "mean: 23.275866442799998 sec\nrounds: 5"
+          }
+        ]
+      }
+    ],
+    "Windows": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a3b54891e8c403d28b7bf2d86443ad310d2d1b51",
+          "message": "Update docs with preferences (#602)",
+          "timestamp": "2023-09-19T19:53:56Z",
+          "tree_id": "b14da6b2792ca95e5305f96c6f291125c7ea2813",
+          "url": "https://github.com/chidiwilliams/buzz/commit/a3b54891e8c403d28b7bf2d86443ad310d2d1b51"
+        },
+        "date": 1695154586996,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.010765156598663974,
+            "unit": "iter/sec",
+            "range": "stddev: 0.12155082047091774",
+            "extra": "mean: 92.89228548000003 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.07509968318898262,
+            "unit": "iter/sec",
+            "range": "stddev: 0.05679658606537646",
+            "extra": "mean: 13.315635399999973 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Faster Whisper - Tiny]",
+            "value": 0.06111068740355496,
+            "unit": "iter/sec",
+            "range": "stddev: 0.035362664630801295",
+            "extra": "mean: 16.36374982000002 sec\nrounds: 5"
           }
         ]
       }
