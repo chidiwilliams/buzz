f0d4fcbaca1c1a016c11daf8293a15d8584f9f02
github-action-benchmark <github@users.noreply.github.com>
2023-10-28 17:37:24 +0000

    add macOS (pytest) benchmark result for eedc2ab2f944df9b03e94c9c115dffabf72968c5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,8 +1,46 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698514641939,
+  "lastUpdate": 1698514644547,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "macOS": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eedc2ab2f944df9b03e94c9c115dffabf72968c5",
+          "message": "Add pip installation (#615)",
+          "timestamp": "2023-10-28T18:32:31+01:00",
+          "tree_id": "8ff84d86a971ca33d5855f591ee56b5048e0bd1f",
+          "url": "https://github.com/chidiwilliams/buzz/commit/eedc2ab2f944df9b03e94c9c115dffabf72968c5"
+        },
+        "date": 1698514634474,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.186261121206612,
+            "unit": "iter/sec",
+            "range": "stddev: 0.20328040750629142",
+            "extra": "mean: 5.368806939000114 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.08038030987025072,
+            "unit": "iter/sec",
+            "range": "stddev: 1.1347680334166506",
+            "extra": "mean: 12.440857737600073 sec\nrounds: 5"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
