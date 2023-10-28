13c55653712abc68c77ee4cd615d0f0ac07e9fa7
github-action-benchmark <github@users.noreply.github.com>
2023-10-28 19:03:35 +0000

    add macOS (pytest) benchmark result for 69d1dd56f2d4fc0a6a5d6f2a2f72a68c998d6d1e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,8 +1,46 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698519812409,
+  "lastUpdate": 1698519815083,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "macOS": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "69d1dd56f2d4fc0a6a5d6f2a2f72a68c998d6d1e",
+          "message": "Add publish PyPI step (#617)",
+          "timestamp": "2023-10-28T18:57:54Z",
+          "tree_id": "da08c5b2dd2ed61e04f7ca877ccd5f43a57b68b8",
+          "url": "https://github.com/chidiwilliams/buzz/commit/69d1dd56f2d4fc0a6a5d6f2a2f72a68c998d6d1e"
+        },
+        "date": 1698519804889,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.15370312117872614,
+            "unit": "iter/sec",
+            "range": "stddev: 2.077492444310569",
+            "extra": "mean: 6.50604875379986 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.0887014629830277,
+            "unit": "iter/sec",
+            "range": "stddev: 0.07197526718312933",
+            "extra": "mean: 11.273771213799956 sec\nrounds: 5"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
