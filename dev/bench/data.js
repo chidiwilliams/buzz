9f74a342385363cfe0035dd19c0cc9c7dd5f5eb9
github-action-benchmark <github@users.noreply.github.com>
2023-11-16 08:43:58 +0000

    add Windows (pytest) benchmark result for 18ed61eec81f932904b3f14273b94ff259cd9997

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,8 +1,53 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700124234891,
+  "lastUpdate": 1700124238337,
   "repoUrl": "https://github.com/chidiwilliams/buzz",
   "entries": {
     "Windows": [
+      {
+        "commit": {
+          "author": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "committer": {
+            "email": "williamschidi1@gmail.com",
+            "name": "Chidi Williams",
+            "username": "chidiwilliams"
+          },
+          "distinct": true,
+          "id": "18ed61eec81f932904b3f14273b94ff259cd9997",
+          "message": "Update whisper.cpp",
+          "timestamp": "2023-11-16T08:13:34Z",
+          "tree_id": "806382d1942e424446b657c533879ef4cf8a1d71",
+          "url": "https://github.com/chidiwilliams/buzz/commit/18ed61eec81f932904b3f14273b94ff259cd9997"
+        },
+        "date": 1700124224597,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper.cpp - Tiny]",
+            "value": 0.013403005278565075,
+            "unit": "iter/sec",
+            "range": "stddev: 0.5183049214840351",
+            "extra": "mean: 74.61013252000002 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Whisper - Tiny]",
+            "value": 0.06171669875679918,
+            "unit": "iter/sec",
+            "range": "stddev: 0.27652125106615355",
+            "extra": "mean: 16.20307015999997 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/transcriber_benchmarks_test.py::test_should_transcribe_and_benchmark[Faster Whisper - Tiny]",
+            "value": 0.10017912749285411,
+            "unit": "iter/sec",
+            "range": "stddev: 0.03273008126923821",
+            "extra": "mean: 9.98211928000003 sec\nrounds: 5"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
