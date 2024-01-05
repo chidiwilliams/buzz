d17677c6bcc0cf96d89909f1204f23627150437d
Chidi Williams <williamschidi1@gmail.com>
2024-01-05 13:10:38 +0000

    fix: execstack

--- tests/widgets/model_type_combo_box_test.py
+++ tests/widgets/model_type_combo_box_test.py
@@ -1,14 +1,34 @@
+import sys
+
+import pytest
+
 from buzz.widgets.model_type_combo_box import ModelTypeComboBox
 
 
 class TestModelTypeComboBox:
-    def test_should_display_items(self, qtbot):
+    @pytest.mark.parametrize(
+        "model_types",
+        [
+            pytest.param(
+                [
+                    "Whisper",
+                    "Whisper.cpp",
+                    "Hugging Face",
+                    "Faster Whisper",
+                    "OpenAI Whisper API",
+                ],
+                pytest.mark.skipif(sys.platform == "linux", reason="Skip on Linux"),
+            ),
+            pytest.param(
+                ["Whisper.cpp", "OpenAI Whisper API"],
+                pytest.mark.skipif(sys.platform != "linux", reason="Skip on non-Linux"),
+            ),
+        ],
+    )
+    def test_should_display_items(self, qtbot, model_types):
         widget = ModelTypeComboBox()
         qtbot.add_widget(widget)
 
-        assert widget.count() == 5
-        assert widget.itemText(0) == "Whisper"
-        assert widget.itemText(1) == "Whisper.cpp"
-        assert widget.itemText(2) == "Hugging Face"
-        assert widget.itemText(3) == "Faster Whisper"
-        assert widget.itemText(4) == "OpenAI Whisper API"
+        assert widget.count() == len(model_types)
+        for index, model_type in enumerate(model_types):
+            assert widget.itemText(index) == model_type
