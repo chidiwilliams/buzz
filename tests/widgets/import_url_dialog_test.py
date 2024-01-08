4013dd307c8fa992c835b70fe8078f99fcc476aa
Chidi Williams <williamschidi1@gmail.com>
2024-01-08 01:37:31 +0000

    feat: import URL for transcription

---
+++ tests/widgets/import_url_dialog_test.py
@@ -0,0 +1,22 @@
+from unittest.mock import patch
+
+from buzz.widgets.import_url_dialog import ImportURLDialog
+
+
+class TestImportURLDialog:
+    def test_should_show_error_with_invalid_url(self, qtbot):
+        dialog = ImportURLDialog()
+        dialog.line_edit.setText("bad-url")
+
+        with patch("PyQt6.QtWidgets.QMessageBox.critical") as mock_critical:
+            dialog.button_box.button(dialog.button_box.StandardButton.Ok).click()
+            mock_critical.assert_called_with(
+                dialog, "Invalid URL", "The URL you entered is invalid."
+            )
+
+    def test_should_return_url_with_valid_url(self, qtbot):
+        dialog = ImportURLDialog()
+        dialog.line_edit.setText("https://example.com")
+
+        dialog.button_box.button(dialog.button_box.StandardButton.Ok).click()
+        assert dialog.url == "https://example.com"
