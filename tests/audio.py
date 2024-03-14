7aec7a902aa23fa42c52e04adfa272b333918599
Chidi Williams <williamschidi1@gmail.com>
2024-03-14 16:55:19 +0000

    test: update timeout

--- tests/audio.py
+++ tests/audio.py
@@ -1,5 +1,5 @@
 import os.path
 
-test_audio_path = os.path.join(
-    os.path.dirname(__file__), "../testdata/whisper-french.mp3"
+test_audio_path = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../testdata/whisper-french.mp3")
 )
