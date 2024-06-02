ddebcccc64a3736ac6720a8319a6fa6737f056e1
Raivis Dejus <raivisd@scandiweb.com>
2024-06-02 12:05:56 +0300

    Adding GPU support for Huggingface models (#772)

--- buzz/transformers_whisper.py
+++ buzz/transformers_whisper.py
@@ -1,16 +1,33 @@
 import sys
+import logging
 from typing import Optional, Union
 
 import numpy as np
 from tqdm import tqdm
 
 import whisper
+import torch
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
+
+def cuda_is_viable(min_vram_gb=10):
+    if not torch.cuda.is_available():
+        return False
+
+    total_memory = torch.cuda.get_device_properties(0).total_memory / 1e9  # Convert bytes to GB
+    if total_memory < min_vram_gb:
+        return False
+
+    return True
 
 
 def load_model(model_name_or_path: str):
     processor = WhisperProcessor.from_pretrained(model_name_or_path)
     model = WhisperForConditionalGeneration.from_pretrained(model_name_or_path)
+
+    if cuda_is_viable():
+        logging.debug("CUDA is available and has enough VRAM, moving model to GPU.")
+        model.to("cuda")
+
     return TransformersWhisper(processor, model)
 
 
@@ -52,7 +69,7 @@ class TransformersWhisper:
                 chunk = audio[seek : seek + self.N_SAMPLES_IN_CHUNK]
                 input_features = self.processor(
                     chunk, return_tensors="pt", sampling_rate=self.SAMPLE_RATE
-                ).input_features
+                ).input_features.to(self.model.device)
                 predicted_ids = self.model.generate(input_features)
                 all_predicted_ids.extend(predicted_ids)
                 text: str = self.processor.batch_decode(
