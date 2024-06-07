7dd2239084d852892160549fc2f10f76830b9c22
Raivis Dejus <raivisd@scandiweb.com>
2024-06-07 11:19:23 +0300

    Fix for recording test

--- tests/transcriber/recording_transcriber_test.py
+++ tests/transcriber/recording_transcriber_test.py
@@ -1,16 +1,15 @@
 from unittest.mock import Mock, patch
 
-import pytest
 from PyQt6.QtCore import QThread
 
 from buzz.model_loader import TranscriptionModel, ModelType, WhisperModelSize
 from buzz.transcriber.recording_transcriber import RecordingTranscriber
 from buzz.transcriber.transcriber import TranscriptionOptions, Task
 from tests.mock_sounddevice import MockInputStream
+from tests.model_loader import get_model_path
 
 
 class TestRecordingTranscriber:
-    @pytest.mark.skip(reason="Hanging")
     def test_should_transcribe(self, qtbot):
         thread = QThread()
 
@@ -18,15 +17,18 @@ class TestRecordingTranscriber:
             model_type=ModelType.WHISPER_CPP, whisper_model_size=WhisperModelSize.TINY
         )
 
+        model_path = get_model_path(transcription_model)
         transcriber = RecordingTranscriber(
             transcription_options=TranscriptionOptions(
                 model=transcription_model, language="fr", task=Task.TRANSCRIBE
             ),
             input_device_index=0,
             sample_rate=16_000,
+            model_path=model_path,
         )
         transcriber.moveToThread(thread)
 
+        thread.started.connect(transcriber.start)
         thread.finished.connect(thread.deleteLater)
 
         mock_transcription = Mock()
@@ -35,13 +37,14 @@ class TestRecordingTranscriber:
         transcriber.finished.connect(thread.quit)
         transcriber.finished.connect(transcriber.deleteLater)
 
-        with patch("sounddevice.InputStream", side_effect=MockInputStream), patch(
-            "sounddevice.check_input_settings"
-        ), qtbot.wait_signal(transcriber.transcription, timeout=60 * 1000):
+        with (patch("sounddevice.InputStream", side_effect=MockInputStream),
+              patch("sounddevice.check_input_settings"),
+              qtbot.wait_signal(transcriber.transcription, timeout=60 * 1000)):
             thread.start()
 
         with qtbot.wait_signal(thread.finished, timeout=60 * 1000):
-            transcriber.stop_recording()
+            if transcriber is not None:
+                transcriber.stop_recording()
 
         text = mock_transcription.call_args[0][0]
         assert "Bienvenue dans Passe" in text
