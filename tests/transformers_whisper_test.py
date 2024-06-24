a4a6fa4f9f03efd845879fbfb41c336d6ea26578
Raivis Dejus <orvils@gmail.com>
2024-06-24 23:44:20 +0300

    Switching to pipeline for HF whisper

--- tests/transformers_whisper_test.py
+++ tests/transformers_whisper_test.py
@@ -1,7 +1,7 @@
 import platform
 import pytest
 
-from buzz.transformers_whisper import load_model
+from buzz.transformers_whisper import TransformersWhisper
 from tests.audio import test_audio_path
 
 
@@ -11,7 +11,7 @@ from tests.audio import test_audio_path
 )
 class TestTransformersWhisper:
     def test_should_transcribe(self):
-        model = load_model("openai/whisper-tiny")
+        model = TransformersWhisper("openai/whisper-tiny")
         result = model.transcribe(
             audio=test_audio_path, language="fr", task="transcribe"
         )
