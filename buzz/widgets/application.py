a8921208aac37651fda71e94df3dc05b2aa151f6
Raivis Dejus <orvils@gmail.com>
2024-07-02 22:10:46 +0300

    Adding ability to change application font size

--- buzz/widgets/application.py
+++ buzz/widgets/application.py
@@ -8,7 +8,8 @@ from buzz.db.dao.transcription_dao import TranscriptionDAO
 from buzz.db.dao.transcription_segment_dao import TranscriptionSegmentDAO
 from buzz.db.db import setup_app_db
 from buzz.db.service.transcription_service import TranscriptionService
-from buzz.settings.settings import APP_NAME
+from buzz.settings.settings import APP_NAME, Settings
+
 from buzz.transcriber.transcriber import FileTranscriptionTask
 from buzz.widgets.main_window import MainWindow
 
@@ -22,8 +23,16 @@ class Application(QApplication):
         self.setApplicationName(APP_NAME)
         self.setApplicationVersion(VERSION)
 
+        self.settings = Settings()
+        font_size = self.settings.value(
+            key=Settings.Key.FONT_SIZE, default_value=self.font().pointSize()
+        )
+
         if sys.platform == "darwin":
-            self.setFont(QFont("SF Pro", self.font().pointSize()))
+            self.setFont(QFont("SF Pro", font_size))
+        else:
+            self.setFont(QFont(self.font().family(), font_size))
+
 
         db = setup_app_db()
         transcription_service = TranscriptionService(
