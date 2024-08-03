3828b55a60b7b325a080e977e468f5a7200a80bb
Raivis Dejus <orvils@gmail.com>
2024-08-03 20:29:27 +0300

    Will try to re-enable test on macOS

--- tests/transformers_whisper_test.py
+++ tests/transformers_whisper_test.py
@@ -1,11 +1,17 @@
 import platform
 import pytest
+import time
+import gc
 
 from buzz.transformers_whisper import TransformersWhisper
 from tests.audio import test_audio_path
 
 
 class TestTransformersWhisper:
+    # @pytest.mark.skipif(
+    #     platform.system() == "Darwin",
+    #     reason="Not supported on Darwin",
+    # )
     def test_should_transcribe(self):
         model = TransformersWhisper("openai/whisper-tiny")
         result = model.transcribe(
@@ -13,3 +19,10 @@ class TestTransformersWhisper:
         )
 
         assert "Bienvenue dans Passe" in result["text"]
+
+        # Cleanup
+        del model
+        del result
+        gc.collect()
+
+        time.sleep(5)
