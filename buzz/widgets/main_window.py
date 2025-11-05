760304c0a804e6314e1e43fffb780e9c40a9dbcc
Raivis Dejus <orvils@gmail.com>
2025-11-05 08:46:00 +0200

    Fix for tests hanging

--- buzz/widgets/main_window.py
+++ buzz/widgets/main_window.py
@@ -425,7 +425,7 @@ class MainWindow(QMainWindow):
 
         self.transcriber_worker.stop()
         self.transcriber_thread.quit()
-        self.transcriber_thread.wait()
+        self.transcriber_thread.wait(3000)  # Wait up to 3 seconds
 
         if self.transcription_viewer_widget is not None:
             self.transcription_viewer_widget.close()
