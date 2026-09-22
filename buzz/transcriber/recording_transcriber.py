8df6fdce68d8a3f06ed49071d34f4225c12adb26
Raivis Dejus <orvils@gmail.com>
2026-09-22 08:02:55 +0300

    Fix for recording transcriber

--- buzz/transcriber/recording_transcriber.py
+++ buzz/transcriber/recording_transcriber.py
@@ -481,7 +481,6 @@ class RecordingTranscriber(QObject):
 
         cmd = [
             server_path,
-            "--host", "0.0.0.0",
             "--port", str(self._whisper_server_port),
             "--inference-path", "/audio/transcriptions",
             "--threads", str(os.getenv("BUZZ_WHISPERCPP_N_THREADS", (os.cpu_count() or 8) // 2)),
